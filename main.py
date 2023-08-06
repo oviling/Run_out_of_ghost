c9ddcb1fe5c819964a0398975d3985f37fbc7720
mishanato <99412583+mishanato@users.noreply.github.com>
2023-08-07 00:59:46 +0300

    0.6
    добавлена абилка для кота с куулдауном, сделаны огранечители игрового поля, добавлен модуль рандом

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random as r
 
 pg.init()
 clock = pg.time.Clock()
@@ -8,51 +9,51 @@ pg.display.set_icon(pg.image.load("textures/ghost-a.png"))
 screen.fill((114, 157, 224))
 
 
-def ghost_move(keys, ghost_x, ghost_y, player_speed):
-    if keys[pg.K_a]:
+def ghost_move(keys, ghost_x, ghost_y, ghost_reload, player_speed):
+    if keys[pg.K_a] and ghost_x > 0:
         ghost_x -= player_speed
         return ghost_x, ghost_y
-    elif keys[pg.K_d]:
+    elif keys[pg.K_d] and ghost_x < 920:
         ghost_x += player_speed
         return ghost_x, ghost_y
-    elif keys[pg.K_s]:
+    elif keys[pg.K_s] and ghost_y < 700:
         ghost_y += player_speed
         return ghost_x, ghost_y
-    elif keys[pg.K_w]:
+    elif keys[pg.K_w] and ghost_y > 0:
         ghost_y -= player_speed
         return ghost_x, ghost_y
     else:
         return ghost_x, ghost_y
 
 
-def cat_move(keys, cat_x, cat_y, player_speed):
-    if keys[pg.K_LEFT]:
+def cat_move(keys, cat_x, cat_y, cat_reload,  ghost_x, ghost_y, player_speed):
+    if keys[pg.K_LEFT] and cat_x > 0:
         cat_x -= player_speed
-        return cat_x, cat_y
-    elif keys[pg.K_RIGHT]:
+        return cat_x, cat_y, ghost_x, ghost_y, cat_reload
+    elif keys[pg.K_RIGHT] and cat_x < 920:
         cat_x += player_speed
-        return cat_x, cat_y
-    elif keys[pg.K_DOWN]:
+        return cat_x, cat_y, ghost_x, ghost_y, cat_reload
+    elif keys[pg.K_DOWN] and cat_y < 700:
         cat_y += player_speed
-        return cat_x, cat_y
-    elif keys[pg.K_UP]:
+        return cat_x, cat_y, ghost_x, ghost_y, cat_reload
+    elif keys[pg.K_UP] and cat_y > 0:
         cat_y -= player_speed
-        return cat_x, cat_y
-    #elif keys[pg.K_SPACE]:
-    #    cat_ability(ghost_x, ghost_y)
+        return cat_x, cat_y, ghost_x, ghost_y, cat_reload
+    elif keys[pg.K_SPACE] and cat_reload > 300:
+        ghost_x, ghost_y = 0, 0
+        cat_reload = 0
+        return cat_x, cat_y, ghost_x, ghost_y, cat_reload
     else:
-        return cat_x, cat_y
-
-
-    #def cat_ability(ghost_x, ghost_y):
-    #    ghost_x, ghost_y = 0, 0
+        return cat_x, cat_y, ghost_x, ghost_y, cat_reload
 
 
 player_speed = 9
-cat_x = 0
-cat_y = 0
+cat_x = 920
+cat_y = 700
+cat_reload = 0
 ghost_x = 0
 ghost_y = 0
+ghost_reload = 0
 
 go_sound = pg.mixer.Sound("sounds/goo.mp3")
 go_sound.play()
@@ -66,14 +67,17 @@ while running:
 
     keys = pg.key.get_pressed()
 
-    cat_x, cat_y = cat_move(keys, cat_x, cat_y, player_speed)
-    ghost_x, ghost_y = ghost_move(keys, ghost_x, ghost_y, player_speed)
+    ghost_x, ghost_y = ghost_move(keys, ghost_x, ghost_y, ghost_reload, player_speed)
+    cat_x, cat_y, ghost_x, ghost_y, cat_reload = cat_move(keys, cat_x, cat_y, cat_reload, ghost_x, ghost_y, player_speed)
 
     screen.blit(bg, (0, 0))
     screen.blit(ghost, (ghost_x, ghost_y))
     screen.blit(cat, (cat_x, cat_y))
 
     pg.display.update()
+
+    cat_reload += 1
+    ghost_reload += 1
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
