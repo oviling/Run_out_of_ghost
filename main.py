0b43c1046101500194e29f9f26f60a662cc658f8
mishanato <99412583+mishanato@users.noreply.github.com>
2023-08-07 01:47:29 +0300

    0.6.5
    попытался сделать рестарт но он не работает, я устал + пора спать, утро вечера мудренее. спокойной ночи!

--- main.py
+++ main.py
@@ -47,37 +47,61 @@ def cat_move(keys, cat_x, cat_y, cat_reload,  ghost_x, ghost_y, player_speed):
         return cat_x, cat_y, ghost_x, ghost_y, cat_reload
 
 
-player_speed = 9
-cat_x = 920
-cat_y = 700
-cat_reload = 0
-ghost_x = 0
-ghost_y = 0
-ghost_reload = 0
+def restart():
+    player_speed = 6
+    cat_x = 920
+    cat_y = 700
+    cat_reload = 0
+    ghost_x = 0
+    ghost_y = 0
+    ghost_reload = 0
+    gameplay = True
+
+    return player_speed, cat_x, cat_y, cat_reload, ghost_x, ghost_y, ghost_reload, gameplay
+
+player_speed, cat_x, cat_y, cat_reload, ghost_x, ghost_y, ghost_reload, gameplay = restart()
 
 go_sound = pg.mixer.Sound("sounds/goo.mp3")
+
+
+myfont = pg.font.Font("font/Roboto-Italic.ttf", 70)
+gameover_text = myfont.render("Для перезапуска нажмите r", True, "Purple")
+
+bg = pg.image.load("textures/Woods.png").convert()
+ghost = pg.transform.scale(pg.image.load("textures/ghost-a.png").convert_alpha(), (54, 102))
+cat = pg.transform.scale(pg.image.load("textures/costume2.png").convert_alpha(), (85, 102))
+
 go_sound.play()
-
-bg = pg.image.load("textures/Woods.png")
-ghost = pg.transform.scale(pg.image.load("textures/ghost-a.png"), (54, 102))
-cat = pg.transform.scale(pg.image.load("textures/costume2.png"), (85, 102))
-
+gameplay = True
 running = True
 while running:
 
     keys = pg.key.get_pressed()
 
-    ghost_x, ghost_y = ghost_move(keys, ghost_x, ghost_y, ghost_reload, player_speed)
-    cat_x, cat_y, ghost_x, ghost_y, cat_reload = cat_move(keys, cat_x, cat_y, cat_reload, ghost_x, ghost_y, player_speed)
+    if gameplay:
 
-    screen.blit(bg, (0, 0))
-    screen.blit(ghost, (ghost_x, ghost_y))
-    screen.blit(cat, (cat_x, cat_y))
+        ghost_x, ghost_y = ghost_move(keys, ghost_x, ghost_y, ghost_reload, player_speed)
+        cat_x, cat_y, ghost_x, ghost_y, cat_reload = cat_move(keys, cat_x, cat_y, cat_reload, ghost_x, ghost_y, player_speed)
+
+        screen.blit(bg, (0, 0))
+        screen.blit(ghost, (ghost_x, ghost_y))
+        screen.blit(cat, (cat_x, cat_y))
+
+        cat_rect = cat.get_rect(topleft=(cat_x, cat_y))
+        ghost_rect = ghost.get_rect(topleft=(ghost_x, ghost_y))
+
+        if cat_rect.colliderect(ghost_rect):
+            gameplay = False
+
+        cat_reload += 1
+        ghost_reload += 1
+    else:
+        screen.blit(gameover_text, (25, 350))
+        if keys[pg.K_r]:
+            restart()
+
 
     pg.display.update()
-
-    cat_reload += 1
-    ghost_reload += 1
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
